429dbac3ffbd575e513e0d146be64ea2f45cd9b8
ArijitPatra2906 <patraarijit440@gmail.com>
2025-02-07 19:12:38 +0530

    industry insights page

--- app/(main)/dashboard/layout.js
+++ app/(main)/dashboard/layout.js
@@ -1,19 +1,12 @@
-import { BarLoader } from "react-spinners";
-import { Suspense } from "react";
-
 export default function Layout({ children }) {
   return (
     <div className="px-5">
       <div className="flex items-center justify-between mb-5">
-        <h1 className="text-5xl font-bold tracking-tight gradient-title">
+        <h1 className="text-3xl font-bold md:text-5xl lg:text-6xl xl:text-7xl gradient-title animate-gradient">
           Industry Insights
         </h1>
       </div>
-      <Suspense
-        fallback={<BarLoader className="pt-4" width={"100%"} color="#9333ea" />}
-      >
-        {children}
-      </Suspense>
+      <div>{children}</div>
     </div>
   );
 }
